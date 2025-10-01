268480639bf0b0fcd686047445efdf2b4188dcfd
tkv4uk <annatachuk54@gmail.com>
2025-10-01 21:26:46 +0300

    add files

---
+++ main.py
@@ -0,0 +1,36 @@
+import math
+
+
+def calc(a, b, op):
+    if op not in '+-*/^%√':
+        return 'Пожалуйста, выберите тип операции: "+, -, *, /, ^, %, √"!'
+
+    if op == '+':
+        return f'{a} + {b} = {a + b}'
+    if op == '-':
+        return f'{a} - {b} = {a - b}'
+    if op == '*':
+        return f'{a} * {b} = {a * b}'
+    if op == '/':
+        return f'{a} / {b} = {a / b}' if b != 0 else 'Ошибка: деление на ноль!'
+    if op == '^':
+        return f'{a} ^ {b} = {a ** b}'
+    if op == '%':
+        return f'{b}% от {a} = {a * b / 100}'
+    if op == '√':
+        return f'√{a} = {math.sqrt(a)}'
+
+
+def main():
+    a = float(input('Пожалуйста, введите первое число: '))
+    op = input('Какой вид операции Вы желаете осуществить?\nВыберите между "+, -, *, /, ^, %, √" : ')
+
+    if op == '√':
+        print(calc(a, 0, op))
+    else:
+        b = float(input('Пожалуйста, введите второе число: '))
+        print(calc(a, b, op))
+
+
+if __name__ == '__main__':
+    main()
